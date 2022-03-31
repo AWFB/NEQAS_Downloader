9993dfa0c61330f79e252908091dfaa9b154c963
Antony Beardsmore <sider@me.com>
2022-03-31 21:36:22 +0100

    Create project

---
+++ NEQAS_Downloader.py
@@ -0,0 +1,46 @@
+from numpy import full
+import dictionary as dic
+
+url = "https://results.ukneqas.org.uk/output/"
+end_of_url = '/10072/report.pdf'
+
+menu_options = {
+    1: 'Vitamin D',
+    2: 'Sweat Tests',
+    3: 'Option 3',
+    4: 'Exit',
+}
+
+def print_menu():
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def vitamin_D ():
+    dist = input('What distribution do you require? ')
+    print(url + dic.VITD['url_code'] + dist + end_of_url)
+    exit()
+
+def sweat_test():
+    dist = input('What distribution do you require? ')
+    print(url + dic.SWEAT['url_code'] + dist + end_of_url)
+
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        
+        #Check what choice was entered and act accordingly
+        if option == 1:
+            vitamin_D()
+        elif option == 2:
+            sweat_test()
+            break
+        elif option == 3:
+            print('Thanks message before exiting')
+            exit()
+        else:
+            print('Invalid option')
---
+++ dictionary.py
@@ -0,0 +1,5 @@
+VITD = {'name': 'Vitamin D', 'url_code':'QEHVITD/'}
+SWEAT = {'name': 'Sweat testing', 'url_code':'QEHSWT/'}
+
+# scheme_1 = Scheme('Vitamin D', 'QEHVITD/')
+# scheme_2 = Scheme('Sweat testing', 'QEHSWT/')
