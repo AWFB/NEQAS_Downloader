f5e43b2359e02bd9656517e69a276b550efc7315
Michael Baldry <michael@brightbits.co.uk>
2022-04-01 09:15:19 +0100

    How I'd do it, with no python knowledge really

--- NEQAS_Downloader.py
+++ NEQAS_Downloader.py
@@ -1,45 +1,56 @@
-from numpy import full
-import dictionary as dic
+url_prefix = "https://results.ukneqas.org.uk/output"
+url_suffix = '10072/report.pdf'
 
-url = "https://results.ukneqas.org.uk/output/"
-end_of_url = '/10072/report.pdf'
+def handle_download_url(opts):
+    dist = input('What distribution do you require? ')
+    print()
+
+    url = '/'.join([url_prefix, opts['url_code'], dist, url_suffix])
+    print(url)
+
+def handle_exit(opts):
+    # this one doesn't need the opts at all, but we have a unified way of calling an option function, so it needs be the same
+    print('Thanks message before exiting')
+    exit()
+
+def read_option():
+    try:
+        option = int(input('Enter your choice: '))
+        print()
+
+        return menu_options[option]
+    except:
+        print('Wrong input. Please enter a number ...')
+        return read_option()
 
 menu_options = {
-    1: 'Vitamin D',
-    2: 'Sweat Tests',
-    3: 'Exit',
+    1: {
+        'name': 'Vitamin D',
+        'url_code': 'QEHVITD',
+        'handler': handle_download_url, # a reference to the function
+    },
+    2: {
+        'name': 'Sweat Tests',
+        'url_code': 'QEHSWT',
+        'handler': handle_download_url,
+    },
+    3: {
+        'name': 'Exit',
+        'url_code': 'QEHVITD/',
+        'handler': handle_exit,
+    }
 }
 
 def print_menu():
     for key in menu_options.keys():
-        print (key, '--', menu_options[key] )
-
-# Fetch url code from dictionary 
-def vitamin_D ():
-    dist = input('What distribution do you require? ')
-    print(url + dic.VITD['url_code'] + dist + end_of_url)
-    exit()
-
-def sweat_test():
-    dist = input('What distribution do you require? ')
-    print(url + dic.SWEAT['url_code'] + dist + end_of_url)
+        print (key, '--', menu_options[key]['name'] )
 
 if __name__=='__main__':
     while(True):
         print_menu()
-        option = ''
-        try:
-            option = int(input('Enter your choice: '))
-        except:
-            print('Wrong input. Please enter a number ...')
-        
-        #Check what choice was entered and act accordingly
-        if option == 1:
-            vitamin_D()
-        elif option == 2:
-            sweat_test()
-        elif option == 3:
-            print('Thanks message before exiting')
-            exit()
-        else:
-            print('Invalid option')
+
+        option = read_option()
+        # call the function in the option, passing in the option, so you can get the 'url_code' or other options inside it
+        option['handler'](option)
+
+        print()
