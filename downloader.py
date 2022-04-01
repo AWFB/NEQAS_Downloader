e77f9fecbcb53f7a2e773698d8e9f3d89ca6a5d0
Antony Beardsmore <sider@me.com>
2022-04-01 17:07:36 +0100

    Added code to download and rename file

--- downloader.py
+++ downloader.py
@@ -1,7 +1,11 @@
-import requests 
+import yaml
+import requests
 
 url_prefix = "https://results.ukneqas.org.uk/output"
 url_suffix = '10072/report.pdf'
+conf = yaml.load(open('LoginDetails.yml'))
+user = conf['user']['user_name']
+password = conf['user']['password']
 
 def handle_download_url(opts):
     dist = input('What distribution do you require? ')
@@ -10,8 +14,11 @@ def handle_download_url(opts):
     url = '/'.join([url_prefix, opts['url_code'], dist, url_suffix])
     print(url)
 
-    r = requests.get(url)
-    filename = url.split('/')[-1] # this will take only -1 splitted part of the url
+    r = requests.get(url, auth=(user, password), allow_redirects=True)
+    
+    filename = ' '.join([opts['name'], dist, '.pdf'])
  
     with open(filename,'wb') as output_file:
         output_file.write(r.content)
+    
+
